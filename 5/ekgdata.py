fd1bc18cded6a90dab5286344fc1969e3ec30fd8
carinatilg <tc4640@mci4me.at>
2024-06-03 10:28:29 +0200

    debugging

--- 5/ekgdata.py
+++ 5/ekgdata.py
@@ -2,8 +2,9 @@ import json
 import pandas as pd
 from person import Person
 import plotly.express as px
-from scipy.signal import find_peaks   
+#from scipy.signal import find_peaks   
 import matplotlib.pyplot as plt
+import numpy as np
 
 # %% Objekt-Welt
 
@@ -11,20 +12,6 @@ import matplotlib.pyplot as plt
 
 class EKGdata:
 
-## Konstruktor der Klasse soll die Daten einlesen
-
-    def __init__(self, ekg_dict):
-        pass
-        self.id = ekg_dict["id"]
-        self.date = ekg_dict["date"]
-        self.data = ekg_dict["result_link"]
-        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV','Time in ms',])
-        self.peaks = self.find_peaks()
-        #self.estimate_HR = self.estimate_hr()
-        self.estimated_hr = self.estimate_hr()
-        self.fig = self.plot_time_series()
-
-# %% Test
     @staticmethod
     def load_by_id(such_id):
     # für alle ekg test über alle personen
@@ -38,67 +25,90 @@ class EKGdata:
         
         for eintrag in person_data:
             for ekg_test in eintrag["ekg_tests"]:
-                if str(ekg_test["id"]) == such_id:
+                if ekg_test["id"] == such_id:
                     return ekg_test
         return {}
-    
+
+    @staticmethod
+    def find_peaks(series, threshold, respacing_factor=5):
+        """
+        A function to find the peaks in a series
+        Args:
+            - series (pd.Series): The series to find the peaks in
+            - threshold (float): The threshold for the peaks
+            - respacing_factor (int): The factor to respace the series
+        Returns:
+            - peaks (list): A list of the indices of the peaks
+        """
+        # Respace the series
+        series = series.iloc[::respacing_factor]
+        
+        # Filter the series
+        series = series[series>threshold]
 
 
-    def find_peaks(self):
-        # Find peaks in the EKG data
-        peaks  = find_peaks(self.df['EKG in mV'], height=330, distance=200, prominence=60)
+        peaks = []
+        last = 0
+        current = 0
+        next = 0
+
+        for index, row in series.items():
+            last = current
+            current = next
+            next = row
+
+            if last < current and current > next and current > threshold:
+                peaks.append(index-respacing_factor)
+
         return peaks
-    
 
-    '''def estimate_hr(self, peaks, sampling_rate):
-        # Calculate time differences between peaks
-        time_difference = [(peaks[i] - peaks[i-1]) / sampling_rate for i in range(1, len(peaks))]
-        # Calculate average time difference
-        avg_time_difference = sum(time_difference) / len(time_difference)
-        # Calculate heart rate (beats per minute)
-        heart_rate = 60 / avg_time_difference
-
-        return heart_rate'''
-    
-    #tobi
-    
-    def estimate_hr(self):
-        self.estimated_hr_list = []
-        peaks_times = self.df['Time in ms'][self.peaks]
-        for i in range(1, len(peaks_times)):
-            difference = (peaks_times.iloc[i] - peaks_times.iloc[i-1])/500*60
-            self.estimated_hr_list.append(difference)
-        self.estimated_hr = sum(self.estimated_hr_list)/len(self.estimated_hr_list)
-        return self.estimated_hr
-    
-    '''def plot_time_series(self, peaks):
+    @staticmethod
+    def plot_ekg(df, peaks):
         df.loc[:, "is_peak"] = False
         df.loc[peaks, "is_peak"] = True
-        fig = px.scatter(df.iloc[0:5000], x='Time in ms', y='EKG in mV', color='is_peak') 
-        return fig 
-    '''
-
-    def plot_time_series(self):
-        fig, ax = plt.subplots()
-        ax.set_xlabel('Time in ms')
-        ax.set_ylabel('EKG in mV')
-        ax.set_title('EKG Time Series with Peaks')
-        ax.plot(self.df['Time in ms'], self.df['EKG in mV'])
-        ax.scatter(self.df['Time in ms'][self.peaks], self.df['EKG in mV'][self.peaks], color='red')
+        fig = px.scatter(df, x='Time in ms', y='EKG in mV', color='is_peak') 
         return fig
-'''
-        plt.figure(figsize=(12, 6))
-        plt.plot(self.df['Time in ms'], self.df['EKG in mV'], color='blue')
-        plt.scatter(self.df['Time in ms'][self.peaks], self.df['EKG in mV'][self.peaks], color='red', marker='x')
-        plt.xlabel('Time in ms')
-        plt.ylabel('EKG in mV')
-        plt.title('EKG Time Series with Peaks')
-        plt.show()'''
-        
-      
+    
+    @staticmethod
+    def plot_hr(df_hr): 
+        fig = px.line(df_hr, x='Time in s', y='Heart Rate in bpm')
+        return fig
+    
+## Konstruktor der Klasse soll die Daten einlesen
 
-        
+    def __init__(self, ekg_dict):
+        pass
+        self.id = ekg_dict["id"]
+        self.date = ekg_dict["date"]
+        self.data = ekg_dict["result_link"]
+        #self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV','Time in ms',])
+        #self.peaks = self.find_peaks()
+        #self.estimate_HR = self.estimate_hr()
+        #self.estimated_hr = self.estimate_hr()
+        #self.fig = self.plot_time_series()
 
+
+    def get_df(self):
+        df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV','Time in ms',])
+        df['Time in s'] = df['Time in ms'] / 1000
+        return df    
+        
+    
+    @staticmethod
+    def estimate_hr(peaks, sampling_rate):
+        # Calculate time differences between peaks
+        time_difference = np.array([(peaks[i] - peaks[i-1]) / sampling_rate for i in range(1, len(peaks))])
+        # Calculate average time difference
+        # avg_time_difference = sum(time_difference) / len(time_difference)
+        # Calculate heart rate (beats per minute)
+        heart_rate = 60. / time_difference
+        time_array = np.array(peaks[1:]) / sampling_rate
+
+        # Create dataframe
+        df_hr = pd.DataFrame({'Time in s': time_array, 'Heart Rate in bpm': heart_rate})
+        return df_hr
+    
+   
 if __name__ == "__main__":
     print("This is a module with some functions to read the EKG data")
     file = open("data/person_db.json")
@@ -106,20 +116,24 @@ if __name__ == "__main__":
     ekg_dict = person_data[0]["ekg_tests"][0]
     print(ekg_dict)
     ekg = EKGdata(ekg_dict)
-    print(ekg.df.head())
     #------------------------------------------------------------
-    ekg_data = EKGdata.load_by_id(1)
-    print("Eintrag von gewählter ID EKG:", ekg_data) #id
+    id_from_selectionbox = 1
+    ekg_dict = EKGdata.load_by_id(id_from_selectionbox)
+    print("Eintrag von gewählter ID EKG:", ekg_dict) #id
+    ekg_data = EKGdata(ekg_dict)
     #------------------------------------------------------------
-    df = pd.read_csv(r'data/ekg_data/01_Ruhe.txt', sep='\t', header=None, names=['EKG in mV','Time in ms',])
-    peaks, _ = find_peaks(df['EKG in mV'], height=0)
+    df = ekg_data.get_df()
+    print(df.head())
+    peaks = EKGdata.find_peaks(df['EKG in mV'], 340, 5)
+    #------------------------------------------------------------
+    fig = EKGdata.plot_ekg(df, peaks)
+    fig.show()
     
-    
     #------------------------------------------------------------
-    heart_rate = ekg.estimate_hr(peaks, 1000)
-    print("Heart Rate: ", heart_rate)
+    df_hr = EKGdata.estimate_hr(peaks, 1000)
+    print(df_hr.head())
     #------------------------------------------------------------
-    fig = ekg.plot_time_series(peaks)
+    fig = EKGdata.plot_hr(df_hr)
     fig.show() 
     #------------------------------------------------------------
 
